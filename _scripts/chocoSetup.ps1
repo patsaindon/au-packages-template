8eb126631abd969060ed921b7e73a007c33ba7b7
Patrick Saindon <pat.saindon@gmail.com>
2022-12-07 11:37:18 -0500

    Update chocoSetup.ps1

--- _scripts/chocoSetup.ps1
+++ _scripts/chocoSetup.ps1
@@ -2,4 +2,5 @@ If (!(Test-Path $env:ChocolateyInstall\license )){ New-Item $env:ChocolateyInsta
 If (!(Test-Path $env:ChocolateyInstall\extensions\chocolatey )){ New-Item $env:ChocolateyInstall\extensions\chocolatey -Type Directory }
 If (!(Test-Path $env:ChocolateyInstall\license\chocolatey.license.xml )){ Copy-Item _scripts/chocolatey.license.xml $env:ChocolateyInstall\license }
 #choco upgrade chocolatey -yf --no-progress
-choco install chocolatey.extension -y
+choco install au chocolatey.extension -y --no-progress
+
